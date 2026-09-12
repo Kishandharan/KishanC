260afecd6fa1ee0d92fcbf48cbdb5fbbfb98fd26
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-09-12 17:32:13 +0530

    Did some changes. Not complete yet

--- Electronics_Emulator/main.c
+++ Electronics_Emulator/main.c
@@ -37,22 +37,22 @@ void setWrAddr(MEM *mem, bool bit1, bool bit2){
   mem->WR_ADDR[1] = bit2;
   
   if((mem->nWE) == false){
-    if(((mem->WR_ADDR[0]) == 0 && (mem->WR_ADDR[1]) == 0)){
+    if(bit1 == 0 && bit2 == 0){
       mem->MEMORY[0][0] = mem->DATA[0];
       mem->MEMORY[0][1] = mem->DATA[1];
       mem->MEMORY[0][2] = mem->DATA[2];
       mem->MEMORY[0][3] = mem->DATA[3];
-    }else if(((mem->WR_ADDR[0]) == 0 && (mem->WR_ADDR[1]) == 1)){
+    }else if(bit1 == 0 && bit2 == 1){
       mem->MEMORY[1][0] = mem->DATA[0];
       mem->MEMORY[1][1] = mem->DATA[1];
       mem->MEMORY[1][2] = mem->DATA[2];
       mem->MEMORY[1][3] = mem->DATA[3];
-    }else if(((mem->WR_ADDR[0]) == 1 && (mem->WR_ADDR[1]) == 0)){
+    }else if(bit1 == 1 && bit2 == 0){
       mem->MEMORY[2][0] = mem->DATA[0];
       mem->MEMORY[2][1] = mem->DATA[1];
       mem->MEMORY[2][2] = mem->DATA[2];
       mem->MEMORY[2][3] = mem->DATA[3];
-    }else if(((mem->WR_ADDR[0]) == 1 && (mem->WR_ADDR[1]) == 1)){
+    }else if(bit1 == 1 && bit2 == 1){
       mem->MEMORY[3][0] = mem->DATA[0];
       mem->MEMORY[3][1] = mem->DATA[1];
       mem->MEMORY[3][2] = mem->DATA[2];
@@ -61,19 +61,35 @@ void setWrAddr(MEM *mem, bool bit1, bool bit2){
   }
 }
 
+void setReAddr(MEM *mem, bool bit1, bool bit2){
+  mem->RE_ADDR[0] = bit1;
+  mem->RE_ADDR[1] = bit2;
+
+  if((mem->nRE) == 0){
+    if(bit1 == 0 && bit2 == 0){
+      mem->OUT[0] = mem->MEMORY[0][0];
+      mem->OUT[1] = mem->MEMORY[0][1];
+      mem->OUT[2] = mem->MEMORY[0][2];
+      mem->OUT[3] = mem->MEMORY[0][3];
+    }else if(bit1 == 0 && bit2 == 1){
+      mem->OUT[0] = mem->MEMORY[1][0];
+      mem->OUT[1] = mem->MEMORY[1][1];
+      mem->OUT[2] = mem->MEMORY[1][2];
+      mem->OUT[3] = mem->MEMORY[1][3];
+    }else if(bit1 == 1 && bit2 == 0){
+      mem->OUT[0] = mem->MEMORY[2][0];
+      mem->OUT[1] = mem->MEMORY[2][1];
+      mem->OUT[2] = mem->MEMORY[2][2];
+      mem->OUT[3] = mem->MEMORY[2][3];
+    }else if(bit1 == 1 && bit2 == 1){
+      mem->OUT[0] = mem->MEMORY[3][0];
+      mem->OUT[1] = mem->MEMORY[3][1];
+      mem->OUT[2] = mem->MEMORY[3][2];
+      mem->OUT[3] = mem->MEMORY[3][3];
+    }
+  }
+}
+
 int main(){
   MEM mem = memInit();
-  
-  mem.nWE = false;
-  mem.DATA[0] = 1;
-  mem.DATA[1] = 1;
-  mem.DATA[2] = 0;
-  mem.DATA[3] = 1;
-
-  setWrAddr(&mem, false, true);
-
-  printf("%d", mem.MEMORY[1][0]);
-  printf("%d", mem.MEMORY[1][1]);
-  printf("%d", mem.MEMORY[1][2]);
-  printf("%d", mem.MEMORY[1][3]);
 }
