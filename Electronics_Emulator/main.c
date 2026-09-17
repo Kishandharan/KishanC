f429a14aca016b69529fe8208e039560225aa7f6
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-09-17 19:22:40 +0530

    Retured after a long time....

--- Electronics_Emulator/main.c
+++ Electronics_Emulator/main.c
@@ -14,6 +14,8 @@ MEM memInit(){
   MEM mem;
   mem.WR_ADDR[0] = 0;
   mem.WR_ADDR[1] = 0;
+  mem.RE_ADDR[0] = 0;
+  mem.RE_ADDR[1] = 0;
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 4; j++){
       mem.MEMORY[i][j] = 0;
@@ -33,6 +35,8 @@ MEM memInit(){
 }
 
 void setWrAddr(MEM *mem, bool bit1, bool bit2){
+  int readNum = 0;
+
   mem->WR_ADDR[0] = bit1;
   mem->WR_ADDR[1] = bit2;
   
@@ -42,22 +46,35 @@ void setWrAddr(MEM *mem, bool bit1, bool bit2){
       mem->MEMORY[0][1] = mem->DATA[1];
       mem->MEMORY[0][2] = mem->DATA[2];
       mem->MEMORY[0][3] = mem->DATA[3];
+      readNum = 0;
     }else if(bit1 == 0 && bit2 == 1){
       mem->MEMORY[1][0] = mem->DATA[0];
       mem->MEMORY[1][1] = mem->DATA[1];
       mem->MEMORY[1][2] = mem->DATA[2];
       mem->MEMORY[1][3] = mem->DATA[3];
+      readNum = 1;
     }else if(bit1 == 1 && bit2 == 0){
       mem->MEMORY[2][0] = mem->DATA[0];
       mem->MEMORY[2][1] = mem->DATA[1];
       mem->MEMORY[2][2] = mem->DATA[2];
       mem->MEMORY[2][3] = mem->DATA[3];
+      readNum = 2;
     }else if(bit1 == 1 && bit2 == 1){
       mem->MEMORY[3][0] = mem->DATA[0];
       mem->MEMORY[3][1] = mem->DATA[1];
       mem->MEMORY[3][2] = mem->DATA[2];
       mem->MEMORY[3][3] = mem->DATA[3];
+      readNum = 3;
     } 
+  }
+
+  if(mem->nRE == 0){
+    if(mem->RE_ADDR[0] == bit1 && mem->RE_ADDR[1] == bit2){
+      mem->OUT[0] = mem->MEMORY[readNum][0];
+      mem->OUT[1] = mem->MEMORY[readNum][1];
+      mem->OUT[2] = mem->MEMORY[readNum][2];
+      mem->OUT[3] = mem->MEMORY[readNum][3];
+    }
   }
 }
 
@@ -90,6 +107,40 @@ void setReAddr(MEM *mem, bool bit1, bool bit2){
   }
 }
 
+void setNRE(MEM *mem, bool new_nRE){
+  if(new_nRE == 1){
+    mem->nRE = true;
+    mem->OUT[0] = -1;
+    mem->OUT[1] = -1;
+    mem->OUT[2] = -1;
+    mem->OUT[3] = -1;
+  }else{
+    if(mem->RE_ADDR[0] == 0 && mem->RE_ADDR[1] == 0){
+      mem->OUT[0] = mem->MEMORY[0][0];
+      mem->OUT[1] = mem->MEMORY[0][1];
+      mem->OUT[2] = mem->MEMORY[0][2];
+      mem->OUT[3] = mem->MEMORY[0][3];
+    }else if(mem->RE_ADDR[0] == 0 && mem->RE_ADDR[1] == 1){
+      mem->OUT[0] = mem->MEMORY[1][0];
+      mem->OUT[1] = mem->MEMORY[1][1];
+      mem->OUT[2] = mem->MEMORY[1][2];
+      mem->OUT[3] = mem->MEMORY[1][3];
+    }else if(mem->RE_ADDR[0] == 1 && mem->RE_ADDR[1] == 0){
+      mem->OUT[0] = mem->MEMORY[2][0];
+      mem->OUT[1] = mem->MEMORY[2][1];
+      mem->OUT[2] = mem->MEMORY[2][2];
+      mem->OUT[3] = mem->MEMORY[2][3];
+    }else if(mem->RE_ADDR[0] == 1 && mem->RE_ADDR[1] == 1){
+      mem->OUT[0] = mem->MEMORY[3][0];
+      mem->OUT[1] = mem->MEMORY[3][1];
+      mem->OUT[2] = mem->MEMORY[3][2];
+      mem->OUT[3] = mem->MEMORY[3][3];
+    }
+  }
+}
+
 int main(){
   MEM mem = memInit();
+  
+  return 0;
 }
